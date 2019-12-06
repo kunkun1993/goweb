e3b24d91698ad22a9ee30633b1a20db6aa7635d4
huangkun <huangkun@bilibili.com>
2019-12-06 11:45:20 +0800

    2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	app := iris.New()
-	app.HandleDir("/", "./public")
+	app.HandleDir("/public", "./public")
 	tmpl := iris.HTML("./views", ".html")
 	tmpl.Layout("shared/layout.html")
 	tmpl.Reload(true)
