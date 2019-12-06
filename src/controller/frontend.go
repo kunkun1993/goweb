038ebd9cfa37d2a51e31be2e8be4970ce4d2ea83
huangkun <huangkun@bilibili.com>
2019-12-06 16:40:51 +0800

    8

--- src/controller/frontend.go
+++ src/controller/frontend.go
@@ -17,7 +17,8 @@ import (
 func Index(ctx iris.Context) {
 	result, err := access.Index()
 
-	fmt.Printf("%v\n", result)
+	fmt.Printf("%v\n", *result)
+	fmt.Printf("%v\n", &result)
 	return
 	if err != nil {
 		log.Fatal(err)
